12b6479edd68cba8b005fa568d3d9aebf3b793d9
Pradeep nishani <pradeep.nishani@gmail.com>
2018-07-18 00:16:39 +0530

    Basic video list skeleton

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
+import VideoListItem from "./video_list_item";
 
-class VideoList extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (<ul className='col-md-4 list-group' >
-            {this.props.videos.length}
-    </ul>);
-    }
+const VideoList = (props) => {
+
+    const videoItems = props.videos.map((video) => {
+        return <VideoListItem key={video.etag} video={video}   />
+    });
+    return (
+        <ul className="col-md-4 list-group">
+        {videoItems}
+       
+        </ul>
+    );
 }
 
 export default VideoList;
